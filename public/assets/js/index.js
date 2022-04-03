f197efe91bffaf1252c73c93e0a6270bd72137b2
Mike Blackson <mkblcksn@gmail.com>
2022-04-02 20:50:30 -0500

    add uuid, delete id request

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -13,27 +13,28 @@ let saveNoteBtn;
 let newNoteBtn;
 let noteList;
 
-
+//Return to index page
 app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, 'public/index.html'))
 );
+//Return to notes pages
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, 'public/notes.html'))
 );
-
+//Return all notes to JSON after reading db.json
 app.get('/api/notes', (req, res) =>
   res.sendFile(path.join(__dirname, 'db/db.json'))
 );
-
-app.post('/api/reviews', (req, res) => {
-  
-  res.json(`${req.method} request received to add a review`);
-
-  console.info(`${req.method} request received to add a review`);
+// POST request
+app.post('/api/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, 'db/db.json'))
 });
 
+app.delete('/api/notes/:id', (req, res) =>
+  res.sendFile(path.join(__dirname, 'db/db.json'))
+);
 app.listen(PORT, () =>
-  console.log(`Express server listening on port ${PORT}!`)
+  console.log(`Listening for request on port ${PORT}!`)
 );
 
 if (window.location.pathname === '/notes') {
